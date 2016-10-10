5d7c9e8c7991e61df86132a85c1300cfb272f54f
pawanpec <pawanpec@gmail.com>
2016-10-10 17:37:34 +0530

    build tenant 1 app

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const appName=process.argv[2];
+console.log(appName);
 // helpers
 var IS_DEV_ENV = (process.env.NODE_ENV === 'dev');
 var fixPath;
@@ -9,6 +10,7 @@ if (IS_DEV_ENV) {
 } else {
   fixPath = function(basePath, dir) {return dir ? basePath + '/' + dir : basePath};
 }
+
 exports.IS_DEV_ENV = IS_DEV_ENV;
 
 exports.VENDOR_NAME     = 'vendor';
@@ -16,21 +18,24 @@ exports.SERVER_NAME     = 'server';
 exports.BROWSER_NAME    = 'browser';
 exports.WORKER_NAME     = 'worker';
 exports.WORKER_APP_NAME = 'worker_app';
-
 exports.HOST = process.env.HOST || 'localhost';
-exports.PORT = process.env.PORT || 5000;
-
+if(appName=='tenant1'){
+  exports.PORT = process.env.PORT || 5000;
+}
+if(appName=='main'){
+  exports.PORT = process.env.PORT || 4000;
+}
 exports.ROOT_DIR    = fixPath('.', '');
 exports.SRC_DIR     = fixPath(exports.ROOT_DIR, 'src');
 exports.DIST_DIR    = fixPath(exports.ROOT_DIR, 'dist');
 exports.PUBLIC_DIR  = fixPath(exports.DIST_DIR, 'public');
 exports.PRIVATE_DIR = fixPath(exports.DIST_DIR, 'private');
-exports.SERVER_DIR  = fixPath(exports.SRC_DIR,  'app/tenant1/server');
+exports.SERVER_DIR  = fixPath(exports.SRC_DIR,  'app/'+appName+'/server');
 
-exports.SERVER_SOURCE_PATH     = fixPath(exports.SRC_DIR, 'app/tenant1/server/server.ts');
-exports.BROWSER_SOURCE_PATH    = fixPath(exports.SRC_DIR, 'app/tenant1//boot/boot_browser.ts');
-exports.WORKER_SOURCE_PATH     = fixPath(exports.SRC_DIR, 'app/tenant1/boot/boot_worker.ts');
-exports.WORKER_APP_SOURCE_PATH = fixPath(exports.SRC_DIR, 'app/tenant1/boot/boot_worker_app.ts');
+exports.SERVER_SOURCE_PATH     = fixPath(exports.SRC_DIR, 'app/'+appName+'/server/server.ts');
+exports.BROWSER_SOURCE_PATH    = fixPath(exports.SRC_DIR, 'app/'+appName+'//boot/boot_browser.ts');
+exports.WORKER_SOURCE_PATH     = fixPath(exports.SRC_DIR, 'app/'+appName+'/boot/boot_worker.ts');
+exports.WORKER_APP_SOURCE_PATH = fixPath(exports.SRC_DIR, 'app/'+appName+'/boot/boot_worker_app.ts');
 
 exports.VENDOR_DLL_MANIFEST_FILE = 'vendor-manifest.json';
 exports.VENDOR_DLL_MANIFEST_PATH = fixPath(exports.PUBLIC_DIR, exports.VENDOR_DLL_MANIFEST_FILE);
